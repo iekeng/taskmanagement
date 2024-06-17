015ac204824e3407ccc519b11500a39eee764804
iekeng <ekeng7.eb@gmail.com>
2024-06-17 07:12:23 +0100

    feat: add task and user models

---
+++ core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=[
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+        ('Overdue', 'Overdue'),
+    ])
+    priority = models.CharField(max_length=20, choices=[
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High')
+    ])
+    due_date = models.DateTimeField()
+    category = models.CharField(max_length=30)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
